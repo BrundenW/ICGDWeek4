9a42bdb581ac92d7e8e8313e5414beb2e840d879
Braden <bradenwilkes@outlook.com>
2021-08-25 16:45:20 +1000

    100 percent complete
    Completed tasks up to the 100% complete section

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,17 +6,28 @@ public class PrintAndHide : MonoBehaviour
 {
 
     int i;
+    int a;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        a = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ":" + ++i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag == "Blue" && i == a)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 }
